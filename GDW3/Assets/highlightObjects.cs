2ba068109684eb9e5f9944c530e0017869185df1
NuclearCosmo <mac.macivor@uoit.net>
2020-10-02 04:04:35 -0400

    Deletion of object, start of highlight, buggy selection (needs rework)
    title explains itself pretty well honestly
    Just a little too late for me to want to do it right now, or early I guess? idk it's 4am, so tomorrow, or later today I guess, good luck future me.

---
+++ GDW3/Assets/highlightObjects.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class highlightObjects : MonoBehaviour
+{
+    //Need to make a shader for the outline to work
+    Color colour = new Color(0, 1, 1, 1);
+    MeshRenderer render;
+    private bool isClicked = false;
+    private bool isOnMouse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (isOnMouse == false)
+            {
+                isClicked = false; //Deselect the object
+            }
+           
+            isOnMouse = false;
+        }
+
+        if (isClicked)
+        {
+            //render.material.SetFloat("outline", 1.0f);
+            //render.material.SetColor("outline", colour);
+        }
+        else
+        {
+            //render.material.SetFloat("outline", 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.Delete) && isClicked)
+        {
+            Destroy(gameObject);
+        }
+        else if (Input.GetMouseButtonDown(3) && isClicked)
+        {
+            //This will be a drag option for quick placement
+            //Deal with it tomorrow
+        }
+        
+
+    }
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            isClicked = true;
+            isOnMouse = true;
+        }
+    }
+}
