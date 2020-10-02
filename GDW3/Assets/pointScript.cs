0e79aed417a14770a84831224cfab98ffc74a4c8
NuclearCosmo <mac.macivor@uoit.net>
2020-10-02 03:01:32 -0400

    The point is now movable
    the point where things spawn can be moved with right click

---
+++ GDW3/Assets/pointScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 position = transform.position;
+
+            Ray aRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitStuff;
+
+            if (Physics.Raycast (aRay, out hitStuff) == true)
+            {
+                position = hitStuff.point;
+            }
+            transform.position = position;
+        }
+
+    }
+}
